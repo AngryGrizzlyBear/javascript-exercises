4d5135da4c6cec673aa9f767356c24f80f5e5e2b
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2024-06-05 10:51:22 -0700

    Update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+if (times > 0){
+    return string.repeat(times);
+}
+else {
+    return "";
+}
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(passedArray, ...args) {
+    const newArray = []
+passedArray.forEach(element => {
+    if(!args.includes(element)){
+        newArray.push(element)
+    }
+});
+return newArray
 };
 
+//console.log(removeFromArray([1,2,3,4], 1,2))
 // Do not edit below this line
 module.exports = removeFromArray;
