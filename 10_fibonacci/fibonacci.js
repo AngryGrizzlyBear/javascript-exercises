df2d867d9fd106622dadca05fe8d8ee8a515f31a
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2024-07-31 12:19:10 -0700

    Homework assignments completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (countArg) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
+
+    if(count < 0) return("OOPS")
+    if (count === 0) return(0)
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < count; i++){
+        let temp = b;
+
+        b = a + b;
+        a = temp;
+    }
+
+    return b;
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
