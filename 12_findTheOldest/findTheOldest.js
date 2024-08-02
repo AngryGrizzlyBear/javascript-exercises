1eb750a60356a964fc040269431fd929e719baa0
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2024-08-02 12:19:15 -0700

    Saving status

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+    function findTheOldest(people) {
+    people.sort((a, b) => {
+        const lastGuy = a.yearOfBirth - a.yearOfDeath;
+        const nextGuy = b.yearOfBirth - b.yearOfDeath;
+        
+        return lastGuy > nextGuy ? 1 : -1
+    })
+    return people[0]
 };
+
+// people.sort((a, b) => {
+//     const lastGuy = a.yearOfBirth - a.yearOfDeath;
+//     const nextGuy = b.yearOfBirth - b.yearOfDeath;
+//     return lastGuy > nextGuy ? -1 : 1
 
 // Do not edit below this line
 module.exports = findTheOldest;
